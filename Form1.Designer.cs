ad92be86803bf2a6dc7cd14ab712c980cc1010f9
valera <sseagal12@gmail.com>
2022-09-10 15:39:46 +0300

    one

---
+++ Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace ChargeData
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.text = new System.Windows.Forms.Label();
+            this.from = new System.Windows.Forms.Label();
+            this.to = new System.Windows.Forms.Label();
+            this.datefrom = new System.Windows.Forms.DateTimePicker();
+            this.dateto = new System.Windows.Forms.DateTimePicker();
+            this.ok = new System.Windows.Forms.Button();
+            this.fbd = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // text
+            // 
+            this.text.AutoSize = true;
+            this.text.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.text.Location = new System.Drawing.Point(59, 22);
+            this.text.Name = "text";
+            this.text.Size = new System.Drawing.Size(341, 23);
+            this.text.TabIndex = 0;
+            this.text.Text = "Перейменуємо файли на нові дати !)";
+            // 
+            // from
+            // 
+            this.from.AutoSize = true;
+            this.from.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.from.Location = new System.Drawing.Point(21, 77);
+            this.from.Name = "from";
+            this.from.Size = new System.Drawing.Size(321, 21);
+            this.from.TabIndex = 1;
+            this.from.Text = "Виберіть нову дату ( - з якого числа - )";
+            // 
+            // to
+            // 
+            this.to.AutoSize = true;
+            this.to.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.to.Location = new System.Drawing.Point(21, 120);
+            this.to.Name = "to";
+            this.to.Size = new System.Drawing.Size(316, 21);
+            this.to.TabIndex = 2;
+            this.to.Text = "Виберіть нову дату ( - по яке число - )";
+            // 
+            // datefrom
+            // 
+            this.datefrom.CalendarFont = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.datefrom.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.datefrom.Location = new System.Drawing.Point(366, 77);
+            this.datefrom.Name = "datefrom";
+            this.datefrom.Size = new System.Drawing.Size(173, 29);
+            this.datefrom.TabIndex = 3;
+            // 
+            // dateto
+            // 
+            this.dateto.CalendarFont = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dateto.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dateto.Location = new System.Drawing.Point(366, 120);
+            this.dateto.Name = "dateto";
+            this.dateto.Size = new System.Drawing.Size(173, 29);
+            this.dateto.TabIndex = 4;
+            // 
+            // ok
+            // 
+            this.ok.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ok.Location = new System.Drawing.Point(63, 174);
+            this.ok.Name = "ok";
+            this.ok.Size = new System.Drawing.Size(119, 31);
+            this.ok.TabIndex = 5;
+            this.ok.Text = "Поїхали ! ->";
+            this.ok.UseVisualStyleBackColor = true;
+            this.ok.Click += new System.EventHandler(this.ok_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(458, 212);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 14);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Кардіоцентр ©";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(560, 235);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ok);
+            this.Controls.Add(this.dateto);
+            this.Controls.Add(this.datefrom);
+            this.Controls.Add(this.to);
+            this.Controls.Add(this.from);
+            this.Controls.Add(this.text);
+            this.Name = "Form1";
+            this.Text = "П Е Р Е Й М Е Н У В А Л К А ))";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label text;
+        private System.Windows.Forms.Label from;
+        private System.Windows.Forms.Label to;
+        private System.Windows.Forms.DateTimePicker datefrom;
+        private System.Windows.Forms.DateTimePicker dateto;
+        private System.Windows.Forms.Button ok;
+        public System.Windows.Forms.FolderBrowserDialog fbd;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ChargeData
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void ok_Click(object sender, EventArgs e)
+        {
+            DialogResult result = fbd.ShowDialog();
+            DateTime first = datefrom.Value.Date;
+            var fdate = first.ToShortDateString();
+            //string name = "виписані КНП ВРЦСП_в_ЦПМСД №";
+            DateTime sekond = dateto.Value.Date;
+            var sdate = sekond.ToShortDateString();
+
+            if (result == DialogResult.OK)
+            {
+                string[] files = Directory.GetFiles(fbd.SelectedPath);
+                int ind;
+                //int number = 1;
+                for (int i = 0; i < files.Length; i++)
+                {
+                    //ind = files[i].LastIndexOf('\\');
+                    //File.Move(files[i], files[i].Remove(ind + 1) + name + number + " " + fdate + "-" + sdate + ".doc");
+                    //number++;
+                    try
+                    {
+                        ind = files[i].LastIndexOf('№');
+                        File.Move(files[i], files[i].Remove(ind + 2) + " " + fdate + "-" + sdate + ".doc");
+                        //number++;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                        throw;
+                    }
+
+
+                }
+                MessageBox.Show("Готово!");
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
